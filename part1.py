a53395e3db0880c5bf018a8d1ff5714acb074b5a
ZichengH-Davis <ziihuang@ucdavis.edu>
2025-11-11 12:30:59 -0800

    Add files via upload

--- part1.py
+++ part1.py
@@ -1,16 +1,15 @@
 import socket
-import os
 import time
 
 
 label = "tmz"
 encoded = label.encode("ascii")
-print(encoded.hex())
+#print(encoded.hex())
 tmz = f"{len(label):02x}" + encoded.hex()
 
 label = "com"
 encoded = label.encode("ascii")
-print(encoded.hex())
+#print(encoded.hex())
 com = f"{len(label):02x}" + encoded.hex()
 
 id_hex  = "2222"
@@ -29,9 +28,85 @@ qclass_hex = "0001"   # Internet
 payload_hex = id_hex + flag + qdcount + ancount + nscount + arcount + qname_hex + qtype_hex + qclass_hex
 payload = bytes.fromhex(payload_hex) #convert string of hex number to bytes
 
-#print("payload:", payload_hex)
-#print("payload byte:", payload)
 
+
+
+def read_2bytes(msg, off):
+   high = msg[off]
+   low  = msg[off + 1]
+   v = (high << 8) | low
+   return v, off + 2
+
+   
+
+def read_4bytes(msg, off):
+    v = ((msg[off] << 24) |
+         (msg[off + 1] << 16) |
+         (msg[off + 2] << 8) |
+          msg[off + 3])
+    return v, off + 4
+
+
+def skip_name(msg, off):
+    while True:
+        length = msg[off]
+        if length == 0: #end of name
+            return off + 1
+        if (length & 0xC0) == 0xC0: #if the first 2 bits are 11, its a pointer
+            return off + 2
+        off += 1 + length 
+
+
+def parse_data(msg):
+    off = 0
+    _id,off = read_2bytes(msg, off)
+    flags,off = read_2bytes(msg, off)
+    qd,off = read_2bytes(msg, off)
+    an,off = read_2bytes(msg, off)
+    ns,off = read_2bytes(msg, off)
+    ar,off = read_2bytes(msg, off)
+
+
+    for _ in range(qd):
+        off = skip_name(msg, off)
+        off += 4
+
+    ips = []
+
+    def read_rrs(count, off):
+        for _ in range(count):
+            off = skip_name(msg, off)
+            rtype, off = read_2bytes(msg, off)
+            rclass, off = read_2bytes(msg, off)
+            _ttl, off = read_4bytes(msg, off)
+            rdlen, off = read_2bytes(msg, off)
+            rdata = msg[off:off + rdlen]
+            off += rdlen
+
+            # if TYPE=1, CLASS=1, length 4 bytes, its an ip address
+            if rtype == 1 and rclass == 1 and rdlen == 4: 
+                ip1 = rdata[0]
+                ip2 = rdata[1]
+                ip3 = rdata[2]
+                ip4 = rdata[3]
+
+
+                s0 = str(ip1)
+                s1 = str(ip2)
+                s2 = str(ip3)
+                s3 = str(ip4)
+
+
+                ip = s0 + "." + s1 + "." + s2 + "." + s3
+                ips.append(ip)
+
+        return off
+
+    off = read_rrs(an, off)# answers
+    off = read_rrs(ns, off)# authority
+    off = read_rrs(ar, off)# additional
+
+    return ips
 
 
 # specify server host and port to connect to
@@ -39,49 +114,34 @@ SERVER_HOST = '170.247.170.2'
 SERVER_PORT = 53
 
 # open a new datagram socket
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:#root server
+
     client_socket.sendto(payload, (SERVER_HOST, SERVER_PORT))
-    data, addr = client_socket.recvfrom(4096)
-    #print(f"Reply {len(data)} bytes from {addr}")
-    #print(data.hex())
-    data_hex = data.hex()
+    data, addr = client_socket.recvfrom(1024)
+    ips = parse_data(data)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket2: #tld
 
-    #print(data_hex[-8:])
-    ip_hex = data_hex[-8:]
+        client_socket2.sendto(payload, (ips[0], SERVER_PORT))
+        data2, addr2 = client_socket2.recvfrom(1024)
+        ips2 = parse_data(data2)
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket3: #authoritative domain
 
-    ip_first = str(int(ip_hex[0:2], 16))
-    #print(ip_first)
-    ip_second = str(int(ip_hex[2:4], 16))
-    #print(ip_second)
-    ip_third = str(int(ip_hex[4:6], 16))
-    #print(ip_third)
-    ip_fourth = str(int(ip_hex[6:8], 16))
-    #print(ip_fourth)
-
-    ip = ip_first + '.' + ip_second + '.' + ip_third + '.' + ip_fourth
-
-    #print(ip)
-
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket2:
-        client_socket2.sendto(payload, (ip, SERVER_PORT))
-        data2, addr2 = client_socket2.recvfrom(4096)
-        print(f"Reply {len(data2)} bytes from {addr2}")
-        print(data2.hex())
+            client_socket3.sendto(payload, (ips2[0], SERVER_PORT))
+            data3, addr3 = client_socket3.recvfrom(1024)
+            ips3 = parse_data(data3)
+            print(parse_data(data3))
 
 
-        data2_hex = data2.hex()
+http = b"GET / HTTP/1.1\r\nHost: tmz.com\r\nConnection: close\r\n\r\n"
 
-        ip_hex2 = data2_hex[-8:]
+SERVER_HOST_TCP = ips3[0]
+SERVER_PORT_TCP = 80
 
-        ip_first2 = str(int(ip_hex2[0:2], 16))
-        #print(ip_first)
-        ip_second2 = str(int(ip_hex2[2:4], 16))
-        #print(ip_second)
-        ip_third2 = str(int(ip_hex2[4:6], 16))
-        #print(ip_third)
-        ip_fourth2 = str(int(ip_hex2[6:8], 16))
-        #print(ip_fourth)
-        ip2 = ip_first2 + '.' + ip_second2 + '.' + ip_third2 + '.' + ip_fourth2
-        print(ip2)
 
-        
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    s.connect((SERVER_HOST_TCP, SERVER_PORT_TCP))
+
+    s.sendall(http)
+
+    data = s.recv(1024)
